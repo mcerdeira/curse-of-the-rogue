85df03cd27045a5663cbce89136c5f2bb819d6c1
Martin Cerdeira <martincerdeira@gmail.com>
2022-11-22 11:11:31 -0300

    Mas primeros setups

--- src/scripts/Global.gd
+++ src/scripts/Global.gd
@@ -4,3 +4,13 @@ extends Node
 func _input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+
+func pick_random(container):
+	if typeof(container) == TYPE_DICTIONARY:
+		return container.values()[randi() % container.size() ]
+	assert( typeof(container) in [
+			TYPE_ARRAY, TYPE_COLOR_ARRAY, TYPE_INT_ARRAY,
+			TYPE_RAW_ARRAY, TYPE_REAL_ARRAY, TYPE_STRING_ARRAY,
+			TYPE_VECTOR2_ARRAY, TYPE_VECTOR3_ARRAY
+			], "ERROR: pick_random" )
+	return container[randi() % container.size()]
