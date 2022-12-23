60f1f8b4bffdbe449bb0d128b49c1270c271f6e4
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2022-12-23 15:52:12 -0300

    Ajuste de posibilidades

--- src/scenes/plan_sprite.gd
+++ src/scenes/plan_sprite.gd
@@ -1,7 +1,7 @@
 extends AnimatedSprite
 
 func _ready():
-	var posi = ["1", "1", "2", "2", "3", "no", "no"]
+	var posi = ["1", "1", "2", "2", "3", "no", "no", "no", "no"]
 	animation = Global.pick_random(posi)
 
 func _physics_process(delta):
