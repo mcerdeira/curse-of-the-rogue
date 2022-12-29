4c037172641ff6eb5c4f1390bada10cefaf20246
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2022-12-29 11:52:56 -0300

    Progreso leveleo

--- src/scenes/Colums.gd
+++ src/scenes/Colums.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-func _ready():
-	if Global.FLOOR_TYPE != Global.floor_types.altar:
+func _ready():	
+	if Global.FLOOR_TYPE == Global.floor_types.altar:
 		var cs = get_children()
 		for i in range(cs.size()):
-			if Global.altar_level < i:
+			if i < Global.altar_level:
+				cs[i].playing = true
 				cs[i].speed_scale = 2
